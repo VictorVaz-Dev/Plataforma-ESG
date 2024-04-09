eeec3df405fc30a15317b8e618e3fc32516b0a10
Victor_Vaz <63057845+VictorVaz-Dev@users.noreply.github.com>
2024-04-09 15:56:24 -0300

    adicionando api do via cep (necessita correção)

--- projeto-fullstack/src/components/inputForms/InputForms.jsx
+++ projeto-fullstack/src/components/inputForms/InputForms.jsx
@@ -2,12 +2,12 @@ import "./InputForms.css"
 import React from "react"
 
 const inputForms = (props) =>{
-    const {type, label, placeholder, row, id} = props;
+    const {type, label, placeholder, row, id, value} = props;
 
     return(
         <div className="inputForms">
             <label>{label}</label>
-            <input type={type} placeholder={placeholder} rows={row} id={id}></input>
+            <input type={type} placeholder={placeholder} rows={row} id={id} value={value}></input>
         </div>
     )
 }
--- projeto-fullstack/src/pages/contatos/Contatos.jsx
+++ projeto-fullstack/src/pages/contatos/Contatos.jsx
@@ -1,17 +1,57 @@
 import "./Contatos.css"
 import InputForms from "../../components/inputForms/InputForms"
+import Api from "../../services/AxiosConfig"
+import React, {useState} from "react"
 
-export default function Contatos(){
+const Contatos = () => {
+    const [cep, setCep] = useState('');
+    const [Rua, setRua] = useState('');
+    const [Bairro, setBairro] = useState('');
+    const [Cidade, setCidade] = useState('');
+
+    function buscarEndereco(cep){
+        //Verificar se o CEP não está vazio antes de fazer a chamada da API
+        if (cep.trim() === ''){
+            //lida com o caso em que o CEP está vazio, se necessário
+            return;
+        }
+
+        //Fazer a solicitação da API ViaCEP usando o valor atualizado do CEP
+        Api.get(`${cep}/json/`)
+          .then(response => {
+            const dados = response.data;
+            setRua(dados.logradouro);
+            setBairro(dados.bairro);
+            setCidade(dados.localidade);
+          })
+          .catch(error => {
+            console.error('Erro ao buscar endereço:', error.message);
+            // Lida com o erro, se necessário
+          });
+      
+    }
+    
+
     return(
         <div className="divContatos">
             <div className="bodyContatos">
                 <h1>Preencha seus dados para entrarmos em contato!</h1>
-                <InputForms label="Nome" type="text" placeholder="Digite seu nome" id="nome"/>
-                <InputForms label="E-mail" type="text" placeholder="Digite seu e-mail" id="email"/>
-                <InputForms label="CEP" type="text" placeholder="Digite seu CEP" id="Cep"/>
-                <InputForms label="Rua" type="text" placeholder="Digite sua Rua" id="Rua"/>
-                <InputForms label="Bairro" type="text" placeholder="Digite seu Bairro" id="Bairro"/>
-                <InputForms label="Cidade" type="text" placeholder="Digite sua cidade" id="Cidade"/>
+                <InputForms label="Nome" type="text" placeholder="Digite seu nome" id="Nome"/>
+                <InputForms label="E-mail" type="email" placeholder="Digite seu e-mail" id="Email"/>
+                <InputForms label="CEP" 
+                type="text" 
+                placeholder="Digite seu CEP" 
+                value={cep} 
+                id="Cep"
+                onInput={(e) => {
+                    const novoCep = e.target.value;
+                    setCep(novoCep);
+                    buscarEndereco(novoCep);
+                }}
+                />
+                <InputForms label="Rua" type="text" placeholder="Digite sua Rua" value={Rua} id="Rua"/>
+                <InputForms label="Bairro" type="text" placeholder="Digite seu Bairro" value={Bairro} id="Bairro"/>
+                <InputForms label="Cidade" type="text" placeholder="Digite sua cidade" value={Cidade} id="Cidade"/>
                 <div className="inputComentario">
                     <label>Comentário</label>
                     <textarea name="Comentario" placeholder="Deixe sua dúvida aqui" cols="30" rows="5" id="Comentario"></textarea>
@@ -21,3 +61,5 @@ export default function Contatos(){
         </div>
     )
 }
+
+export default Contatos
