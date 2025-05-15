3317c9819f0c375d84418f74b66fd60396220185
Victor_Vaz <63057845+VictorVaz-Dev@users.noreply.github.com>
2025-05-15 15:54:59 -0300

    subindo segurança para as permissões
    .

--- src/services/FirebaseConfig.js
+++ src/services/FirebaseConfig.js
@@ -2,15 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDv2hQIwiHtZrQIRLVeBVP1cLc4a1FtYgA",
-  authDomain: "site-fullstack-fiap.firebaseapp.com",
-  projectId: "site-fullstack-fiap",
-  storageBucket: "site-fullstack-fiap.appspot.com",
-  messagingSenderId: "666420552177",
-  appId: "1:666420552177:web:b05f069e82e05e79eaa450",
-  measurementId: "G-605JNNZL25"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
